ee49ffab5c27c2b54e8f68e1ace198d0e9595603
stenson-serjuz <stenson.group@gmail.com>
2026-05-06 11:59:34 +0900

    Update lifecycle.py

--- bootstrap/lifecycle.py
+++ bootstrap/lifecycle.py
@@ -1,15 +1,43 @@
+from enum import Enum, auto
+
 from bootstrap.container import Container
+
+
+class LifecycleState(Enum):
+    INITIAL = auto()
+    STARTING = auto()
+    RUNNING = auto()
+    STOPPING = auto()
+    STOPPED = auto()
 
 
 class LifecycleManager:
     def __init__(self, container: Container) -> None:
         self._container = container
+        self._state = LifecycleState.INITIAL
+
+    @property
+    def state(self) -> LifecycleState:
+        return self._state
 
     async def startup(self) -> None:
+        if self._state is not LifecycleState.INITIAL:
+            raise RuntimeError("Invalid lifecycle transition to startup")
+
+        self._state = LifecycleState.STARTING
+
         self._container.init_config()
         self._container.init_logging()
         self._container.init_logger_factory()
 
+        self._state = LifecycleState.RUNNING
+
     async def shutdown(self) -> None:
+        if self._state is not LifecycleState.RUNNING:
+            raise RuntimeError("Invalid lifecycle transition to shutdown")
+
+        self._state = LifecycleState.STOPPING
+
         # future: close pools
-        pass
+
+        self._state = LifecycleState.STOPPED
