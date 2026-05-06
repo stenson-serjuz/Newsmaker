3577c377813547d75ae2a95f41fb8a95f79cdb0e
stenson-serjuz <stenson.group@gmail.com>
2026-05-06 14:46:22 +0900

    Update errors.py

--- parsers/base/errors.py
+++ parsers/base/errors.py
@@ -8,3 +8,9 @@ class RetryableParserError(ParserError):
 
 class FatalParserError(ParserError):
     pass
+
+
+class HTTPStatusError(RetryableParserError):
+    def __init__(self, status_code: int) -> None:
+        super().__init__(f"http_status:{status_code}")
+        self.status_code = status_code
