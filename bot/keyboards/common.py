153c864d34b2442ce6cd3eb8ca83ca8f0ca1be90
stenson-serjuz <stenson.group@gmail.com>
2026-05-06 13:11:10 +0900

    Update common.py

--- bot/keyboards/common.py
+++ bot/keyboards/common.py
@@ -4,6 +4,6 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 def back_keyboard() -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text="⬅️ Back", callback_data="nav:back")]
+            [InlineKeyboardButton(text="⬅️", callback_data="nav:back")]
         ]
     )
