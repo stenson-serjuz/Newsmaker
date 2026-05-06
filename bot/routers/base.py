ac9aea37647ca65eddb393672c0c871523d97ed6
stenson-serjuz <stenson.group@gmail.com>
2026-05-06 13:11:58 +0900

    Update base.py

--- bot/routers/base.py
+++ bot/routers/base.py
@@ -1,6 +1,15 @@
 from aiogram import Router
 
+from bot.routers.admin import build_admin_router
+from bot.routers.registration import router as registration_router
+from bot.handlers.start import router as start_router
 
-def build_base_router() -> Router:
-    router = Router(name="base")
+
+def build_root_router() -> Router:
+    router = Router(name="root")
+
+    router.include_router(start_router)
+    router.include_router(registration_router)
+    router.include_router(build_admin_router())
+
     return router
