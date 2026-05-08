e312f69655f17c0299227e0ea198e526061026bc
stenson-serjuz <stenson.group@gmail.com>
2026-05-08 11:30:46 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,17 +3,26 @@ from __future__ import annotations
 import asyncio
 
 from bootstrap.application import Application
+from core.logging.logger import get_logger
+
+
+logger = get_logger()
 
 
 async def main() -> None:
+    logger.info("app_runtime_starting")
+
     app = Application()
+
+    logger.info("application_created")
 
     await app.start()
 
-    try:
-        await app.run()
-    finally:
-        await app.stop()
+    logger.info("application_start_completed")
+
+    await app.run()
+
+    logger.info("application_run_completed")
 
 
 if __name__ == "__main__":
